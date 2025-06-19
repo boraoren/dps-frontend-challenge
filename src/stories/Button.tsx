76fda6359172e4cb7a6ba74d2e9293c34047f78a
boraoren <boraoren@gmail.com>
2025-06-19 19:39:20 +0300

    fix(lint): issues for storybook implementation #17

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import './button.css';
 
 export interface ButtonProps {
@@ -17,21 +15,21 @@ export interface ButtonProps {
 
 /** Primary UI component for user interaction */
 export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
+	primary = false,
+	size = 'medium',
+	backgroundColor,
+	label,
+	...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
-  );
+	const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+	return (
+		<button
+			type="button"
+			className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+			style={{ backgroundColor }}
+			{...props}
+		>
+			{label}
+		</button>
+	);
 };
