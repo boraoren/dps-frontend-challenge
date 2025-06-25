cacec09b89e1d56556a8213edc618453a47f8ec7
boraoren <boraoren@gmail.com>
2025-06-26 00:13:57 +0300

    style(components/table/list/item): align left table data #57

--- src/blocks/Card/UserList/Container/index.stories.tsx
+++ src/blocks/Card/UserList/Container/index.stories.tsx
@@ -5,11 +5,11 @@ const meta = {
 	title: 'DPS/Blocks/Card/UserList/Container',
 	component: CardUserListContainer,
 	parameters: {
-		layout: 'centered'
+		layout: 'centered',
 	},
 	tags: ['autodocs'],
 	argTypes: {},
-	args: {}
+	args: {},
 } satisfies Meta<typeof CardUserListContainer>;
 
 export default meta;
@@ -20,36 +20,41 @@ export const Integration: Story = {
 		filter: {
 			name: {
 				title: 'Name',
-				titleBold: true
+				titleBold: true,
 			},
 			select: {
 				title: 'City',
 				titleBold: true,
-				placeHolder: 'Select City'
+				placeHolder: 'Select City',
 			},
 
 			checkbox: {
-				title: 'Highlight oldest per city'
-			}
+				title: 'Highlight oldest per city',
+			},
 		},
 		options: {
-			//TODO this is just hack, when merge firstName and lastName it maps to name. Need to add name to select to show in header.
-			//fix using https://github.com/boraoren/dps-frontend-challenge/issues/55
-			select: ['name','firstName', 'lastName', 'address.city', 'birthDate'],
+			select: [
+				'name',
+				'firstName',
+				'lastName',
+				'address.city',
+				'birthDate',
+			],
+
 			concat: [
 				{
 					values: ['firstName', 'lastName'],
-					to: 'name'
-				}, {
-					//TODO this is just hack, its purpose is to merge columns by given using values.
-					//fix using https://github.com/boraoren/dps-frontend-challenge/issues/55
+					to: 'name',
+				},
+				{
 					values: ['birthDate'],
-					to: 'Birthday'
-				}
-			]
+					to: 'Birthday',
+				},
+
+			],
 		},
 		pagination: {
-			limit: 10
-		}
-	}
+			limit: 10,
+		},
+	},
 };
